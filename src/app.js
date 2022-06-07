d79eeeac26316c0834d64228b8f3a50c7fc7ea87
DianaB177 <anaidb77@gmail.com>
2022-06-07 18:29:52 +0200

    Commit todo

---
+++ src/app.js
@@ -0,0 +1,101 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+function displayForecast(response) {
+  let forecats = response.data.daily;
+  let forecatsElement = document.querySelector("#forecast");
+  let forecatsHTML = `<div class='row'>`;
+  forecats.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecatsHTML =
+        forecatsHTML +
+        `<div class= 'col-2'>
+            <div class='weather-forecast-date'>${formatDay(
+              forecastDay.dt
+            )}</div>
+            <img src='http://openweatherthermap.org/img/wn${
+              forecastDay.weather[0].icon
+            }@2x.png'
+            alt='' width=42/>
+            <div class='weather-forecast-temperatures'>
+            <span class='weather-forecast-temperatures-max'>
+            ${Math.round(forecatsDay.temp.max)}º</span>
+            <span class='weather-forecast-temperatures-min'>
+            ${Math.round(forecatsDay.temp.min)}º</span>
+            </div>
+            </div>`;
+    }
+  });
+  forecatsHTML = forecatsHTML + `</div>`;
+  forecatsElement.innerHTML = forecatsHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+  celsiusTemperature = response.data.main.temp;
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
+}
+function search(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("New York");
